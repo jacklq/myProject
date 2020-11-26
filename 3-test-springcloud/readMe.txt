8dfb8db7aad7bcae1bf037ad9e417e3ddbcdc190
jacklq <877016605@qq.com>
2020-11-26 14:35:38 +0800

    增加Hystrix服务保护

---
+++ 3-test-springcloud/readMe.txt
@@ -0,0 +1,11 @@
+2020-11-20
+     开始springcloud项目，增加注册中心和网关
+
+2020-11-26  https://www.jianshu.com/p/cce702d44b7d
+    增加Hystrix服务保护，
+        注意在pom中引入Hystrix服务保护时，注意其版本
+         <dependency>
+                    <groupId>org.springframework.cloud</groupId>
+                    <artifactId>spring-cloud-starter-netflix-hystrix</artifactId>
+                    <version>2.0.2.RELEASE</version>
+         </dependency>
--- 3-test-springcloud/service-a/src/main/java/com/example/servicea/Controller/TestController.java
+++ 3-test-springcloud/service-a/src/main/java/com/example/servicea/Controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.servicea.Controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,19 @@ public class TestController {
 
     @Value( "${server.port}")
     private  String port;
+
+    /**在接口上开启Hystrix服务保护*/
+    @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping("/hello")
     public String hello() {
+        try{
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         return "hello world"+port;
+    }
+   public String fallback(){
+        return  "服务器繁忙";
     }
 }
